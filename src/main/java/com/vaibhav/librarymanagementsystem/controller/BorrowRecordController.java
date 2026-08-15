bc29d02bb84e7443cc97505b879cdab6c2244114
VaibhavRajSingh <vaibhavrajsingh009@gmail.com>
2026-08-15 21:18:24 +0530

    Added BorrowRecordController

---
+++ src/main/java/com/vaibhav/librarymanagementsystem/controller/BorrowRecordController.java
@@ -0,0 +1,24 @@
+package com.vaibhav.librarymanagementsystem.controller;
+
+import com.vaibhav.librarymanagementsystem.entity.BorrowRecord;
+import com.vaibhav.librarymanagementsystem.service.BorrowRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/borrow")
+public class BorrowRecordController {
+
+    @Autowired
+    private BorrowRecordService borrowRecordService;
+
+    @PostMapping("/{bookId}/member/{memberId}")
+    public BorrowRecord borrowBook(@PathVariable Long bookId, @PathVariable Long memberId) {
+        return borrowRecordService.borrowBook(bookId, memberId);
+    }
+
+    @PutMapping("/return/{recordId}")
+    public BorrowRecord returnBook(@PathVariable Long recordId) {
+        return borrowRecordService.returnBook(recordId);
+    }
+}
